79a6dca1a1e25ac9c535f812e5dc6ad598d25aec
tgd1973 <tgd1973@yahoo.com>
2020-05-03 14:53:50 +0200

    Create vesta-elfm.sh

---
+++ vesta-elfm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Made by Sam Wattoo
+# Copyright WapKiz Ltd
+# Version: 1.1 
+echo "Lets start..."
+# Let's install the EL file manager Vesta UI!  
+function Vestawpinstaller() { 
+echo "VestaCP Installing EL file manager...." 
+cd /usr/local/vesta/web/list
+wget https://raw.githubusercontent.com/vesta-cp/vestacp-file-manager/master/file%20manager.tar -O elfm.tar
+tar -xf elfm.tar
+rm -rf elfm.tar
+# Add the link to the panel.html file 
+if grep -q 'elfm' /usr/local/vesta/web/templates/admin/panel.html; then
+		echo 'Already there.'
+	else 
+sed -i '/<div class="l-menu clearfix noselect">/a <div class="l-menu__item <?php if($TAB == "elfm" ) echo "l-menu__item--active"; ?>"><a href="/list/elfm/" target="_blank"><?=__("El FM")?></a></div>' /usr/local/vesta/web/templates/admin/panel.html;
+fi
+echo "Done! Check VestaCP!" 
+}
+Vestawpinstaller
